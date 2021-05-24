062cfc803dd602d1a329b5d120975d8d9f932dda
0neir0s <athular7@gmail.com>
2021-05-24 11:27:11 +0530

    Added dijkstras

--- detect-cycles.py
+++ detect-cycles.py
@@ -1,3 +1,5 @@
+from math import inf
+from heapq import heappush, heappop
 from collections import defaultdict
 
 class Graph(object):
@@ -103,3 +105,58 @@ g.addEdge(4, 1)
 g.addEdge(2, 3)
 g.addEdge(3, 1)
 getTopologicalSort(g)
+
+#-------------------------------------------------------------------------
+
+def allTopologies(graph):
+    
+    def dfs():
+        nexts = [ v for v, e in outEdges.items() if (not e) and (v not in visited) ]
+        if not nexts:
+            output.append(list(path))
+        for nxt in nexts:
+            path.append(nxt)
+            visited.add(nxt)
+            for neighbor in graph[nxt]:
+                outEdges[neighbor] -= 1
+            dfs()
+            for neighbor in graph[nxt]:
+                outEdges[neighbor] += 1
+            visited.remove(nxt)
+            path.pop()
+
+    path, output, visited, outEdges = [], [], set(), {v:0 for v in graph}
+    for neighbors in graph.values():
+        for neighbor in neighbors:
+            outEdges[neighbor] += 1
+    dfs()
+    return output
+
+print(allTopologies({0:[], 1:[], 2:[3], 3:[1], 4:[0,1], 5:[0,2]})) 
+
+#-----------------------------------------------------------------------
+
+def dijkstras(graph, source):
+    distances = {vertex: inf for vertex in graph}
+    distances[source] = 0
+    pq = [(0, source)] 
+    while len(pq) > 0:
+        distance, vertex = heappop(pq)
+        if distance > distances[vertex]:
+            continue
+        for neighbor, weight in graph[vertex].items():
+            nDistance = distance + weight
+            if nDistance < distances[neighbor]:
+                distances[neighbor] = distance
+                heappush(pq, (distance, neighbor))
+    return distances
+
+example = {
+    'U': {'V': 2, 'W': 5, 'X': 1},
+    'V': {'U': 2, 'X': 2, 'W': 3},
+    'W': {'V': 3, 'U': 5, 'X': 3, 'Y': 1, 'Z': 5},
+    'X': {'U': 1, 'V': 2, 'W': 3, 'Y': 1},
+    'Y': {'X': 1, 'W': 1, 'Z': 1},
+    'Z': {'W': 5, 'Y': 1},
+}
+print(dijkstras(example, 'X'))
